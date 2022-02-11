5dad35aa770d3287d9d43f0ef8522a4aeda15f99
Oksana <oks.svetlova2014@yandex.ru>
2022-02-12 02:21:32 +0300

    First file in repository

---
+++ lesson1/HomeWorkApp.java
@@ -0,0 +1,63 @@
+package lesson1;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+
+    }
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+
+    }
+
+    public static void checkSumSign() {
+        int a = 0;
+        int b = 9;
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+    public static void printColor() {
+        int value = 78;
+        if (value <=0) {
+            System.out.println("Красный");
+        } else if (value >0 && value<=100) {
+            System.out.println("Жёлтый");
+        } else {
+            System.out.println("Зелёный");
+        }
+    }
+    public static void compareNumbers() {
+        int a =3;
+        int b =35;
+        if (a>=b) {
+            System.out.println("a>=b");
+        } else {
+            System.out.println("a<b");
+        }
+        }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
